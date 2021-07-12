2e1cff7dd5e2c9c9d20306d2a1765a670da714bd
Karan1519 <75117505+Karan1519@users.noreply.github.com>
2021-07-12 16:14:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import DrawerNavigator from "./navigation/drawerNavigator";
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+     <DrawerNavigator/>
+    </NavigationContainer>
+  );
+}
